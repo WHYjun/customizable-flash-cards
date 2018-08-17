b04b08167cb5ca97dc7e7d484fe3250c127e5637
WHYjun <yjunchoi@bu.edu>
2018-08-17 00:12:49 -0400

    set up client side folder structure

---
+++ client/webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path')
+var webpack = require('webpack')
+
+const config = {
+  entry: './index.jsx',
+  output: {
+    path: path.join(__dirname),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  }
+}
+module.exports = config
